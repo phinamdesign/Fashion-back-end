637bc8adae1957fac5a48aa2f990842ffb79c167
Nghiemducnam <Nghiemducnam0312@gmail.com>
2020-01-09 10:48:50 +0700

    created security entity in model

--- src/main/java/com/codegym/repository/UserRepository.java
+++ src/main/java/com/codegym/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.codegym.repository;
 
-public interface UserRepository {
+import java.util.Optional;
+
+import com.codegym.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
---
+++ src/main/java/com/codegym/security/jwt/JwtAuthEntryPoint.java
@@ -0,0 +1,4 @@
+package com.codegym.security.jwt;
+
+public class JwtAuthEntryPoint {
+}
