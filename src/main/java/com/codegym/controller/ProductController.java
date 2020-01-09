be07b968a89360e58ce1c0f4850a631dee496b3a
Phi Nam <phinamhd1992@gmail.com>
2020-01-09 11:44:09 +0700

    getProduct method create

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -5,12 +5,10 @@ import com.codegym.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -26,5 +24,14 @@ public class ProductController {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<?> getProduct(@PathVariable Long id){
+        Optional<Product> product = productService.findById(id);
+        if (!product.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 }
