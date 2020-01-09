9b16a7238dd2625efcd0800b4d67270670a926d7
Phi Nam <phinamhd1992@gmail.com>
2020-01-09 15:14:10 +0700

    add producController

---
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.codegym.controller;
+
+import com.codegym.model.Product;
+import com.codegym.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/product")
+    public ResponseEntity<List<Product>> listProduct(){
+        List<Product> products = (List<Product>) productService.findAll();
+        if (products.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<?> getProduct(@PathVariable Long id){
+        Optional<Product> product = productService.findById(id);
+        if (!product.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+//    @PutMapping("/product")
+//    public ResponseEntity<?> creatrProduct()
+}
