4ea49373bf20b8dae238afb2542f6be6a61853fe
Phi Nam <phinamhd1992@gmail.com>
2020-02-08 16:40:50 +0700

    update supplier

--- src/main/java/com/codegym/model/Supplier.java
+++ src/main/java/com/codegym/model/Supplier.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Entity
 public class Supplier {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long supplierId;
     @NotEmpty
     private String supplierName;
