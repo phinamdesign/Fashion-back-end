83efa5d21f0107cdff9b6acab1735f383b95f2bf
Nghiemducnam <Nghiemducnam0312@gmail.com>
2020-02-10 17:30:29 +0700

    add payment in ndnam branch

---
+++ src/main/java/com/codegym/model/Payment.java
@@ -0,0 +1,39 @@
+package com.codegym.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public Payment() {
+
+    }
+
+    public Payment(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
